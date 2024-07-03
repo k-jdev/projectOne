a0fc3b52917bbe823a510910b3f07190e341906b
k-jdev <svv.ooj@gmail.com>
2024-07-03 11:59:35 +0300

    modified:   src/App.jsx 	modified:   src/main.jsx 	modified:   src/pages/Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <h1>test</h1>
+      <Routes>
+        <Route Component={<Home />} path="/" />
+      </Routes>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./main.css";
-import { BrowserRouter } from "react-router-dom";
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import { BrowserRouter as Router } from "react-router-dom";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Router>
+    <App />
+  </Router>
+);
