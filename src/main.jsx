25da76f5fb791bbd6f787ae32bd49e386dbf44f4
Xenos <svv.ooj@gmail.com>
2024-09-10 09:51:42 +0300

    deleted:    src/features/counter/counterSlice.js 	modified:   src/main.jsx 	deleted:    src/store.js

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./main.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import { store } from "./store.js";
-import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>
+  <Router>
+    <App />
+  </Router>
 );
